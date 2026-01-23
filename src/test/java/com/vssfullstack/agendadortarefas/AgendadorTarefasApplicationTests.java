8f71dc4cb0e8f2e677cbe32d32fb38398fea5a06
Victor <vssfullstack@gmail.com>
2026-01-23 18:12:22 -0300

    commit inicial com esqueleto

---
+++ src/test/java/com/vssfullstack/agendadortarefas/AgendadorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vssfullstack.agendadortarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
