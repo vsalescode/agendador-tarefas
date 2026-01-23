b23fbcbe868e4707e6c6bb102ead8fb89f6087e0
Victor <vssfullstack@gmail.com>
2026-01-23 19:08:12 -0300

    adicionadas configurações de segurança

---
+++ src/main/java/com/vssfullstack/agendadortarefas/business/dto/UsuarioDTO.java
@@ -0,0 +1,16 @@
+package com.vssfullstack.agendadortarefas.business.dto;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UsuarioDTO {
+
+    private String email;
+    private String senha;
+}
